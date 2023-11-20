470574c1206cf6449acd18a427e159732d36369d
Hojin <wkdghwls0623@gmail.com>
2023-11-21 03:56:19 +0900

    Env: 배포오류수정(service.ts, constant.ts 주석처리)

--- src/api/service.ts
+++ src/api/service.ts
@@ -1,32 +1,32 @@
-import axios from 'axios';
-import { CONTENT_TYPE, SERVER_ID, SERVER_URL } from '../constant';
-import { getCookie } from '../util/util';
+// import axios from 'axios';
+// import { CONTENT_TYPE, SERVER_ID, SERVER_URL } from '../constant';
+// import { getCookie } from '../util/util';
 
-const client = axios.create({
-  baseURL: SERVER_URL,
-  headers: {
-    'content-type': CONTENT_TYPE,
-    serverId: SERVER_ID,
-  },
-});
+// const client = axios.create({
+//   baseURL: SERVER_URL,
+//   headers: {
+//     'content-type': CONTENT_TYPE,
+//     serverId: SERVER_ID,
+//   },
+// });
 
-client.interceptors.request.use(
-  (config) => {
-    const accessToken = getCookie('accessToken');
-    if (accessToken) {
-      config.headers['Authorization'] = `Bearer ${accessToken}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  },
-);
+// client.interceptors.request.use(
+//   (config) => {
+//     const accessToken = getCookie('accessToken');
+//     if (accessToken) {
+//       config.headers['Authorization'] = `Bearer ${accessToken}`;
+//     }
+//     return config;
+//   },
+//   (error) => {
+//     return Promise.reject(error);
+//   },
+// );
 
-export const postLogin = async (id: string, password: string) => {
-  const res = await client.post('login', {
-    id: id,
-    password: password,
-  });
-  return res;
-};
+// export const postLogin = async (id: string, password: string) => {
+//   const res = await client.post('login', {
+//     id: id,
+//     password: password,
+//   });
+//   return res;
+// };
