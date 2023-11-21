2b5fbd2c2634fe7217a4e3a9e6af1f89bbda27ef
gaori <monooi@naver.com>
2023-11-21 17:08:32 +0900

    Chore: ThemeProvider 추가

---
+++ src/style/styled.d.ts
@@ -0,0 +1,29 @@
+import 'styled-components';
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    colors: {
+      primary: string;
+      secondary: string;
+      tertiary: string;
+    };
+    fonts: {
+      primary: string;
+      secondary: string;
+    };
+    fontSizes: {
+      xs: string;
+      sm: string;
+      md: string;
+      lg: string;
+      xl: string;
+    };
+    fontWeights: {
+      light: number;
+      regular: number;
+      medium: number;
+      semiBold: number;
+      bold: number;
+    };
+  }
+}
---
+++ src/style/theme.ts
@@ -0,0 +1,29 @@
+import { DefaultTheme } from 'styled-components';
+
+const theme: DefaultTheme = {
+  colors: {
+    primary: '#000',
+    secondary: '#fff',
+    tertiary: '#ccc',
+  },
+  fonts: {
+    primary: 'Helvetica Neue',
+    secondary: 'Helvetica',
+  },
+  fontSizes: {
+    xs: '0.75rem',
+    sm: '0.875rem',
+    md: '1rem',
+    lg: '1.25rem',
+    xl: '1.5rem',
+  },
+  fontWeights: {
+    light: 300,
+    regular: 400,
+    medium: 500,
+    semiBold: 600,
+    bold: 700,
+  },
+};
+
+export default theme;
