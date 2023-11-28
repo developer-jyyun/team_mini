bdf2749f9b7733a7d50997c87b93540a0faf2966
JiHongkyu <hongkyu9941@gmail.com>
2023-11-29 00:57:07 +0900

    Refactor: constant.ts 파일 수정

--- src/constant.ts
+++ src/constant.ts
@@ -1,4 +1,2 @@
-// export const SERVER_URL = import.meta.env.VITE_SERVER_URL;
-// export const SERVER_URL = 'http://52.79.192.222:8080/';
-export const SERVER_URL = 'https://mocks-server.net';
+export const SERVER_URL = import.meta.env.VITE_SERVER_URL;
 export const CONTENT_TYPE = 'application/json';
