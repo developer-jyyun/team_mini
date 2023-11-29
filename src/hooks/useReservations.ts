f7c731939919165b88be70446e9f8ffc5b8c73dd
gaori <monooi@naver.com>
2023-11-29 15:24:16 +0900

    Fix: 결제창 API 및 인터페이스 수정

--- src/hooks/useReservations.ts
+++ src/hooks/useReservations.ts
@@ -1,11 +1,11 @@
-import { postAccomodation } from '@/api/service';
+import { getAccommodation } from '@/api/service';
 import { useQueries } from '@tanstack/react-query';
 
 const useReservations = (accommodationId: string[]) => {
   const results = useQueries({
     queries: accommodationId.map((id) => ({
       queryKey: ['accommodation', id],
-      queryFn: () => postAccomodation(id),
+      queryFn: () => getAccommodation(id),
     })),
   });
 
