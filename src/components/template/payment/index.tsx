73d751837a6e6785a9893858bb5bf17fb68f1aac
gaori <monooi@naver.com>
2023-11-29 15:25:01 +0900

    Feat: 예약 정보 결제창으로 가져오기

--- src/components/template/payment/index.tsx
+++ src/components/template/payment/index.tsx
@@ -11,8 +11,32 @@ import PaymentOptions from './PaymentOptions';
 import PaymentReservations from './PaymentReservations';
 import PaymentRoomList from './PaymentRoomList';
 import PaymentTerms from './PaymentTerms';
+import { useRecoilValue } from 'recoil';
+import { orderState, reservationState } from '@/states/atom';
+import { useMutation } from '@tanstack/react-query';
+import { postOrders } from '@/api/service';
+import { OrderRequest } from '@/interfaces/interface';
 
 const PaymentContainer = () => {
+  const reservation = useRecoilValue(reservationState);
+  const orderData = useRecoilValue(orderState);
+
+  const orderRequestBody: OrderRequest = {
+    orders: [{ ...reservation }],
+    payment: orderData.payment,
+  };
+
+  const { mutate } = useMutation({
+    mutationFn: async () => {
+      const orderRequestBody: OrderRequest = {
+        orders: [{ ...reservation }],
+        payment: orderData.payment,
+      };
+      const response = await postOrders(orderRequestBody);
+      return response.data;
+    },
+  });
+
   return (
     <>
       <StyledTitle $mt="4rem" $px="5rem">
