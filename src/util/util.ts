0a599be31010273ca02f52f0c5304219e36366c7
JiHongkyu <hongkyu9941@gmail.com>
2023-11-29 13:16:18 +0900

    Fix: styled-component props에 prefix 추가

--- src/util/util.ts
+++ src/util/util.ts
@@ -1,6 +1,6 @@
 export const setCookie = (accessToken: string) => {
   try {
-    document.cookie = `accessToken=${accessToken}`;
+    document.cookie = `accessToken=${accessToken};max-age=3600;path=/;secure`;
   } catch (err) {
     console.log(err);
     alert('쿠키 설정에 실패했습니다.');
