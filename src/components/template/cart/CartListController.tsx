e6949d7af53ee3a6112002f8687dd803a431b2ea
JiHongkyu <hongkyu9941@gmail.com>
2023-11-29 14:15:35 +0900

    Feat: 장바구니 전체 상품, 선택된 상품 명시적으로 표시

--- src/components/template/cart/CartListController.tsx
+++ src/components/template/cart/CartListController.tsx
@@ -1,3 +1,4 @@
+import { Cart } from '@/interfaces/interface';
 import { StyledDeleteButton } from '@/style/cart/cartStyle';
 import {
   StyledCheckboxInput,
@@ -5,12 +6,26 @@ import {
   StyledText,
 } from '@/style/payment/paymentStyle';
 
-const CartListController = () => {
+interface ICartListControllerProps {
+  cartsData: Cart[];
+  checkedCartsData: Cart[];
+}
+
+const CartListController = ({
+  cartsData,
+  checkedCartsData,
+}: ICartListControllerProps) => {
+  const cartsTotal = cartsData.length !== 0 ? cartsData.length : 0;
+  const checkedCartsTotal =
+    checkedCartsData.length !== 0 ? checkedCartsData.length : 0;
+
   return (
     <StyledFlexContainer style={{ width: '100%' }}>
       <StyledFlexContainer $justifyContent="flex-start" $gap="12px">
         <StyledCheckboxInput type="checkbox" />
-        <StyledText $fontWeight={700}>전체선택(3/3)</StyledText>
+        <StyledText $fontWeight={700}>
+          전체선택({checkedCartsTotal}/{cartsTotal})
+        </StyledText>
       </StyledFlexContainer>
       <StyledDeleteButton>선택삭제</StyledDeleteButton>
     </StyledFlexContainer>
--- src/components/template/cart/index.tsx
+++ src/components/template/cart/index.tsx
@@ -10,6 +10,7 @@ import CartListController from './CartListController';
 import { useEffect, useState } from 'react';
 import { Cart } from '@/interfaces/interface';
 import { getCarts } from '@/api/service';
+import { useNavigate } from 'react-router-dom';
 
 export interface IFormValue {
   name: string;
@@ -19,14 +20,62 @@ export interface IFormValue {
 }
 
 const CartContainer = () => {
+  const navigate = useNavigate();
   const [cartsData, setCartsData] = useState<Cart[]>([]);
   const [checkedCartsData, setCheckedCartsData] = useState<Cart[]>([]);
+
+  const handleSubmit = (): void => {
+    const productIds = checkedCartsData.map((cart) => cart.productId);
+    const queryString = productIds.map((id) => `productId=${id}`).join('&');
+
+    navigate(`/payment?${queryString}`);
+  };
 
   useEffect(() => {
     const fetchData = async (): Promise<void> => {
       try {
         const res = await getCarts();
-        setCartsData(res.data.items);
+        setCartsData([
+          {
+            cartItemId: 1,
+            accommodationId: 1,
+            accomodationName: '신라호텔',
+            accomodationAddress: '제주특별자치도 서귀포시 중문관광로72번길 75',
+            accomodationCategory: 'B02010100',
+            productId: 1,
+            productName: '더블 스탠다드룸',
+            checkIn: '2023-11-21',
+            checkOut: '2023-11-22',
+            personNumber: 2,
+            price: 20000,
+          },
+          {
+            cartItemId: 2,
+            accommodationId: 1,
+            accomodationName: '신라호텔',
+            accomodationAddress: '제주특별자치도 서귀포시 중문관광로72번길 75',
+            accomodationCategory: 'B02010100',
+            productId: 2,
+            productName: '더블 스탠다드룸',
+            checkIn: '2023-11-21',
+            checkOut: '2023-11-22',
+            personNumber: 2,
+            price: 20000,
+          },
+          {
+            cartItemId: 3,
+            accommodationId: 1,
+            accomodationName: '신라호텔',
+            accomodationAddress: '제주특별자치도 서귀포시 중문관광로72번길 75',
+            accomodationCategory: 'B02010100',
+            productId: 3,
+            productName: '더블 스탠다드룸',
+            checkIn: '2023-11-21',
+            checkOut: '2023-11-22',
+            personNumber: 2,
+            price: 20000,
+          },
+        ]);
       } catch (err) {
         console.log(err);
       }
@@ -43,7 +92,10 @@ const CartContainer = () => {
           'Pretendard, system-ui, Avenir, Helvetica, Arial, sans-serif',
       }}>
       <StyledTitle $mt="1.5rem">장바구니</StyledTitle>
-      <CartListController />
+      <CartListController
+        cartsData={cartsData}
+        checkedCartsData={checkedCartsData}
+      />
       <StyledHLine $mBlock="1rem" />
       <CartList
         cartsData={cartsData}
@@ -52,7 +104,10 @@ const CartContainer = () => {
       />
       <StyledHLine $mBlock="1rem" />
       <CartDetail />
-      <StyledButton style={{ width: '100%' }} $variant="primary">
+      <StyledButton
+        style={{ width: '100%' }}
+        $variant="primary"
+        onClick={handleSubmit}>
         결제하기
       </StyledButton>
     </StyledWrapper>
