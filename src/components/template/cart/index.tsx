4109f3d465b66dd632f6e41c7bb0ee5b88a946a5
JiHongkyu <hongkyu9941@gmail.com>
2023-11-29 13:50:02 +0900

    Feat: query string으로 productId 전달

--- src/components/template/cart/index.tsx
+++ src/components/template/cart/index.tsx
@@ -10,6 +10,7 @@ import CartListController from './CartListController';
 import { useEffect, useState } from 'react';
 import { Cart } from '@/interfaces/interface';
 import { getCarts } from '@/api/service';
+import { useNavigate } from 'react-router-dom';
 
 export interface IFormValue {
   name: string;
@@ -19,8 +20,16 @@ export interface IFormValue {
 }
 
 const CartContainer = () => {
+  const navigate = useNavigate();
   const [cartsData, setCartsData] = useState<Cart[]>([]);
   const [checkedCartsData, setCheckedCartsData] = useState<Cart[]>([]);
+
+  const handleSubmit = (): void => {
+    const productIds = checkedCartsData.map((cart) => cart.productId);
+    const queryString = productIds.map((id) => `productId=${id}`).join('&');
+
+    navigate(`/payment?${queryString}`);
+  };
 
   useEffect(() => {
     const fetchData = async (): Promise<void> => {
@@ -52,7 +61,10 @@ const CartContainer = () => {
       />
       <StyledHLine $mBlock="1rem" />
       <CartDetail />
-      <StyledButton style={{ width: '100%' }} $variant="primary">
+      <StyledButton
+        style={{ width: '100%' }}
+        $variant="primary"
+        onClick={handleSubmit}>
         결제하기
       </StyledButton>
     </StyledWrapper>
