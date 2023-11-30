2a063f464e10616bc70eff7c58451cd9f40ea891
Hojin <wkdghwls0623@gmail.com>
2023-11-30 16:01:26 +0900

    Fix: 메인페이지 데이터패칭 예외처리 추가

--- src/components/template/main/index.tsx
+++ src/components/template/main/index.tsx
@@ -3,7 +3,6 @@ import { StyledGridContainer } from '@/style/main/productCardStyle';
 import { ProductCard } from './ProductCard';
 import { getProducts, getProductsCategory } from '@/api/service';
 import { useLocation } from 'react-router-dom';
-import { v4 as uuidv4 } from 'uuid';
 import { mainData } from '@/interfaces/interface';
 
 const MainContainer = () => {
@@ -13,48 +12,44 @@ const MainContainer = () => {
   const location = useLocation();
   const categoryRef = useRef<string | null>(null);
 
-  useEffect(() => {
-    const queryParams = new URLSearchParams(location.search);
-    const newCategory = queryParams.get('category');
-    categoryRef.current = newCategory;
+  async function fetchProducts(categoryParam?: string) {
+    try {
+      const res = categoryParam
+        ? await getProductsCategory(categoryParam)
+        : await getProducts();
+      const productsData = res.data;
 
-    async function fetchProducts(categoryParam?: string) {
-      try {
-        let res;
-        if (categoryParam) {
-          res = await getProductsCategory(categoryParam);
-        } else {
-          res = await getProducts();
-        }
-
-        const productsData = res.data;
-        console.log(productsData);
-
-        if (productsData.length === 0) {
-          setShowNoResults(true);
-          setProductCards([]);
-        } else {
-          setShowNoResults(false);
-          const cards = productsData.map((product: mainData) => (
-            <ProductCard
-              key={uuidv4()}
-              address={product.address}
-              accommodationID={product.accommodationId}
-              imgUrl={product.imageUrl}
-              name={product.name}
-              score={product.score}
-              price={product.price}
-            />
-          ));
-          setProductCards(cards);
-        }
-      } catch (error) {
-        console.error('조회 실패:', error);
+      // 데이터가 배열이고, 비어 있지 않은 경우에만 처리
+      if (Array.isArray(productsData) && productsData.length > 0) {
+        setShowNoResults(false);
+        const cards = productsData.map((product: mainData) => (
+          <ProductCard
+            key={product.accommodationId}
+            address={product.address}
+            accommodationID={product.accommodationId}
+            imgUrl={product.imageUrl}
+            name={product.name}
+            score={product.score}
+            price={product.price}
+          />
+        ));
+        setProductCards(cards);
+      } else {
+        // 빈 배열이거나 유효하지 않은 데이터인 경우
+        setShowNoResults(true);
+        setProductCards([]);
       }
+    } catch (error) {
+      console.error('조회 실패:', error);
+      // 에러 상황에 대한 추가 처리 (예: 에러 메시지 상태 업데이트)
+      setShowNoResults(true);
+      setProductCards([]);
     }
+  }
 
+  useEffect(() => {
     fetchProducts(categoryRef.current as string);
-  }, [location.search]);
+  }, [location.search]); // 의존성 배열 확인
 
   return (
     <>
