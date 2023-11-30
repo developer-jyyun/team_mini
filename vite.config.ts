2a2f5848257d3bbd848fe9bebcea06a73bd47ba6
Hojin <wkdghwls0623@gmail.com>
2023-11-30 23:39:30 +0900

    Remove: 소스맵삭제

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    sourcemap: true,
     minify: 'terser',
     terserOptions: {
       compress: {
