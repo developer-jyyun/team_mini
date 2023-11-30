fc9cd7887c647e905269ad2348f96d0c164224e4
joanShim <joanjeongahshim@gmail.com>
2023-11-30 19:48:43 +0900

    Chore: 미사용 import 삭제

--- src/components/template/main/index.tsx
+++ src/components/template/main/index.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { StyledGridContainer } from '@/style/main/productCardStyle';
 import { ProductCard } from './ProductCard';
-import { getProducts, getProductsCategory } from '@/api/service';
+import { getProducts } from '@/api/service';
 import { useLocation } from 'react-router-dom';
 
 const MainContainer = () => {
